983451ed6b4c289a09ffd191646d37c7055a41e1
unknown <sarah.bakhti.pro@gmail.com>
2025-06-19 08:48:47 +0200

    ajout

--- class/App/Model/Model.php
+++ class/App/Model/Model.php
@@ -4,30 +4,30 @@ namespace App\Model;
 
 abstract class Model {
 
-    protected static string $table;
+    // protected static string $table;
 
-    protected static function create(array $data) {
-        $keys = array_keys($data);
-        $columns = implode(', ',$keys);
-        $values = implode(', :',$keys);
-        $sql = "INSERT INTO " . self::$table . " (" . $columns . ") VALUES (:" . $values . ")";
-        $sql->execute($data);
-    }
+    // protected static function create(array $data) {
+    //     $keys = array_keys($data);
+    //     $columns = implode(', ',$keys);
+    //     $values = implode(', :',$keys);
+    //     $sql = "INSERT INTO " . self::$table . " (" . $columns . ") VALUES (:" . $values . ")";
+    //     $sql->execute($data);
+    // }
 
-    protected static function readOne($id) {
-        $sql = "SELECT * FROM " . self::$table . " WHERE :id=" .$id;
-    }
+    // protected static function readOne($id) {
+    //     $sql = "SELECT * FROM " . self::$table . " WHERE :id=" .$id;
+    // }
 
-    protected static function readAll() {
-        $sql = "SELECT * FROM " . self::$table;
-    }
+    // protected static function readAll() {
+    //     $sql = "SELECT * FROM " . self::$table;
+    // }
 
-    protected static function update() {
+    // protected static function update() {
         
-    }
+    // }
 
-    protected static function delete($id) {
-        $sql = "DELETE FROM " .self::$table . " WHERE :id=" . $id;
-    }
+    // protected static function delete($id) {
+    //     $sql = "DELETE FROM " .self::$table . " WHERE :id=" . $id;
+    // }
 
 }
